e44a8b82f38e87f12282d4405057f0154912f2a1
Oleg Yarin <monapasan@github.com>
2023-12-19 23:20:30 +0100

    update readme & remove comments

--- README.md
+++ README.md
@@ -19,3 +19,9 @@ npm run transferLyxRelayCall
 This script uses relay call to execute a transaction on the LUKSO blockchain and is set up for testing purposes.
 
 
+## What needs to happen
+
+We have an Lukso EOA that should fund transfer transaction.
+As input we have have Up1 that transfers LSP7 token to Up2.
+This transaction is funded by Lukso EOA.
+
--- scripts/transferLyxRelayCall.ts
+++ scripts/transferLyxRelayCall.ts
@@ -64,20 +64,12 @@ async function executeRelayCall() {
   let encodedMessage = solidityPacked(
   ['uint256', 'uint256', 'uint256', 'uint256', 'uint256', 'bytes'],
   [
-    // MUST be number `25`
-    LSP25_VERSION,       // `0x0000000000000000000000000000000000000000000000000000000000000019`
-    // e.g: `4201` for LUKSO Testnet
-    chainId,             // `0x0000000000000000000000000000000000000000000000000000000000001069`
-    // e.g: nonce number 5 of the signer key X 
-    // (the private key associated with the address of the controller that want to execute the payload)
-    nonce,              // `0x0000000000000000000000000000000000000000000000000000000000000005`
-    // e.g: valid until 1st January 2025 at midnight (GMT).
-    // Timestamp = 1735689600
-    validityTimestamps, // `0x0000000000000000000000000000000000000000000000000000000067748580`
-    // e.g: not funding the contract with any LYX (0)
-    msgValue,           // `0x0000000000000000000000000000000000000000000000000000000000000000`
-    // e.g: execute(uint256,address,uint256,bytes) -> send 3 LYX to address `0xcafecafecafecafeafecafecafecafeafecafecafecafeafecafecafecafe`
-    abiPayload,         // `0x44c028fe0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000cafecafecafecafecafecafecafecafecafecafe00000000000000000000000000000000000000000000000029a2241af62c000000000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000000000000`
+    LSP25_VERSION,       
+    chainId,             
+    nonce,              
+    validityTimestamps, 
+    msgValue,           
+    abiPayload,         
   ],
 );
   console.log("encodedMessage received:", encodedMessage);
